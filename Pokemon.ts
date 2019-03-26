fc34c05ed1f5d116439eab09a3c137298c4f5cb4
ChiaLing <chia_ling_lim@dell.com>
2019-03-27 07:06:48 +0800

    20190727 Pokemon coding

--- Pokemon.ts
+++ Pokemon.ts
@@ -3,18 +3,24 @@ import { question } from 'readline-sync'
 const pokemonlist = [
   {
     name: 'Balbasaur',
-    type: 'Grass/Poison',
-    hp: 200
+    type: 'Grass',
+    hp: 200,
+    attack: 20,
+    defence: 30
   },
   {
     name: 'Charmander',
     type: 'Fire',
-    hp: 200
+    hp: 200,
+    attack: 30,
+    defence: 20
   },
   {
     name: 'Squirtle',
     type: 'Water',
-    hp: 200
+    hp: 200,
+    attack: 30,
+    defence: 10
   }
 ]
 
@@ -76,105 +82,118 @@ console.log('-------------------Game Start----------------------')
 listPokemon(pokemonlist)
 let myPokemon = question('Please select a Pokemon. \n')
 console.log('Your pokemon is ' + pokemonlist[myPokemon].name + '.')
-// var myPokemon = pokemonlist[choosePokemon].name
-// var myPokemonType = pokemonlist[choosePokemon].type
 let myPokemonHp = pokemonlist[myPokemon].hp
 
-let oppChoose = Math.floor(Math.random() * pokemonlist.length)
-console.log('Opponent pokemon is ' + pokemonlist[oppChoose].name + '.')
-// var oppPokemon = pokemonlist[oppChoose].name
-// var oppPokemonType = pokemonlist[oppChoose].type
-let oppPokemonHp = pokemonlist[oppChoose].hp
+let oppPokemon = Math.floor(Math.random() * pokemonlist.length)
+console.log('Opponent pokemon is ' + pokemonlist[oppPokemon].name + '.')
+let oppPokemonHp = pokemonlist[oppPokemon].hp
 
 console.log('------------------Battle begins--------------------')
 let myTurn = true
 
 while (myPokemonHp > 0 && oppPokemonHp > 0) {
   if (myTurn) {
+    let skillDamage = 0
+    let totalDamage = 0
+    listSkillsType(pokemonlist[myPokemon].type)
+
     let chooseSkill = question('Please select a skill to attack. \n')
-    let skillDamage
-    var sleepSkill
+
     if (pokemonlist[myPokemon].type === 'Grass') {
-      listSkills(grassSkills)
       console.log(pokemonlist[myPokemon].name + ' attack with ' + grassSkills[chooseSkill].skill + '.')
       //when use sleep powder
-      if (grassSkills[chooseSkill].skill != 'Sleep Powder') {
-        if (pokemonlist[oppChoose].type === 'Fire') {
+      if (grassSkills[chooseSkill].skill != 'Sleep powder') {
+        if (pokemonlist[oppPokemon].type === 'Fire') {
           skillDamage = grassSkills[chooseSkill].damage / 2
+          totalDamage = totalDamageCount(pokemonlist[myPokemon].attack, pokemonlist[oppPokemon].defence, skillDamage)
         } else {
           skillDamage = grassSkills[chooseSkill].damage
+          totalDamage = totalDamageCount(pokemonlist[myPokemon].attack, pokemonlist[oppPokemon].defence, skillDamage)
         }
-        oppPokemonHp -= skillDamage
+        oppPokemonHp -= totalDamage
       } else {
-        sleepSkill = grassSkills[chooseSkill].skill
+        myTurn = true
       }
     }
     else if (pokemonlist[myPokemon].type === 'Fire') {
-      listSkills(fireSkills)
       console.log(pokemonlist[myPokemon].name + ' attack with ' + fireSkills[chooseSkill].skill + '.')
-      if (pokemonlist[oppChoose].type === 'Water') {
-        skillDamage = grassSkills[chooseSkill].damage / 2
-      } else if (pokemonlist[oppChoose].type === 'Grass') {
-        skillDamage = grassSkills[chooseSkill].damage * 2
+      if (pokemonlist[oppPokemon].type === 'Water') {
+        skillDamage = fireSkills[chooseSkill].damage / 2
+        totalDamage = totalDamageCount(pokemonlist[myPokemon].attack, pokemonlist[oppPokemon].defence, skillDamage)
+      } else if (pokemonlist[oppPokemon].type === 'Grass') {
+        skillDamage = fireSkills[chooseSkill].damage * 2
+        totalDamage = totalDamageCount(pokemonlist[myPokemon].attack, pokemonlist[oppPokemon].defence, skillDamage)
       } else {
-        skillDamage = grassSkills[chooseSkill].damage
+        skillDamage = fireSkills[chooseSkill].damage
+        totalDamage = totalDamageCount(pokemonlist[myPokemon].attack, pokemonlist[oppPokemon].defence, skillDamage)
       }
-      oppPokemonHp -= skillDamage
+      oppPokemonHp -= totalDamage
     }
     else if (pokemonlist[myPokemon].type === 'Water') {
-      listSkills(waterSkills)
       console.log(pokemonlist[myPokemon].name + ' attack with ' + waterSkills[chooseSkill].skill + '.')
-      if (pokemonlist[oppChoose].type === 'Fire') {
-        skillDamage = grassSkills[chooseSkill].damage * 2
+      if (pokemonlist[oppPokemon].type === 'Fire') {
+        skillDamage = waterSkills[chooseSkill].damage * 2
+        totalDamage = totalDamageCount(pokemonlist[myPokemon].attack, pokemonlist[oppPokemon].defence, skillDamage)
       } else {
-        skillDamage = grassSkills[chooseSkill].damage
+        skillDamage = waterSkills[chooseSkill].damage
+        totalDamage = totalDamageCount(pokemonlist[myPokemon].attack, pokemonlist[oppPokemon].defence, skillDamage)
       }
-      oppPokemonHp -= waterSkills[chooseSkill].damage
+      oppPokemonHp -= totalDamage
     }
-    if (sleepSkill != 'Sleep powder') {
-      console.log(pokemonlist[oppChoose].name + ' HP has reduce to ' + oppPokemonHp + '.')
+
+    if (grassSkills[chooseSkill].skill != 'Sleep powder') {
+      console.log(pokemonlist[oppPokemon].name + ' HP has reduce to ' + oppPokemonHp + '.')
       myTurn = false
     } else {
-      console.log(pokemonlist[oppChoose].name + ' has fall asleep and missed a chance!')
+      console.log(pokemonlist[oppPokemon].name + ' has fall asleep and missed a chance!')
       myTurn = true
     }
-    console.log('----------------------------------------------------------------------------')
+    console.log('-----------------------------------------------------')
   } else {
-    let oppSkillDamage
-    var oppSleepSkill
-    if (pokemonlist[oppChoose].type === 'Grass') {
+    let oppSkillDamage = 0
+    let oppTotalDamage = 0
+    let oppSleepSkill
+    if (pokemonlist[oppPokemon].type === 'Grass') {
       let oppChooseSkill = Math.floor(Math.random() * grassSkills.length)
-      if (grassSkills[oppChooseSkill].skill != 'Sleep Powder') {
-        console.log(pokemonlist[oppChoose].name + ' attack with ' + grassSkills[oppChooseSkill].skill + '.')
+      if (grassSkills[oppChooseSkill].skill != 'Sleep powder') {
+        console.log(pokemonlist[oppPokemon].name + ' attack with ' + grassSkills[oppChooseSkill].skill + '.')
         if (pokemonlist[myPokemon].type === 'Fire') {
           oppSkillDamage = grassSkills[oppChooseSkill].damage / 2
+          oppTotalDamage = totalDamageCount(pokemonlist[oppPokemon].attack, pokemonlist[myPokemon].defence, oppSkillDamage)
         } else {
           oppSkillDamage = grassSkills[oppChooseSkill].damage
+          oppTotalDamage = totalDamageCount(pokemonlist[oppPokemon].attack, pokemonlist[myPokemon].defence, oppSkillDamage)
         }
-        myPokemonHp -= oppSkillDamage
+        myPokemonHp -= oppTotalDamage
       } else {
         oppSleepSkill = grassSkills[oppChooseSkill].skill
+        myTurn = false
       }
-    } else if (pokemonlist[oppChoose].type === 'Fire') {
+    } else if (pokemonlist[oppPokemon].type === 'Fire') {
       let oppChooseSkill = Math.floor(Math.random() * fireSkills.length)
-      console.log(pokemonlist[oppChoose].name + ' attack with ' + fireSkills[oppChooseSkill].skill + '.')
-      if (pokemonlist[oppChoose].type === 'Water') {
-        oppSkillDamage = grassSkills[oppChooseSkill].damage / 2
-      } else if (pokemonlist[oppChoose].type === 'Grass') {
-        oppSkillDamage = grassSkills[oppChooseSkill].damage * 2
+      console.log(pokemonlist[oppPokemon].name + ' attack with ' + fireSkills[oppChooseSkill].skill + '.')
+      if (pokemonlist[myPokemon].type === 'Water') {
+        oppSkillDamage = fireSkills[oppChooseSkill].damage / 2
+        oppTotalDamage = totalDamageCount(pokemonlist[oppPokemon].attack, pokemonlist[myPokemon].defence, oppSkillDamage)
+      } else if (pokemonlist[myPokemon].type === 'Grass') {
+        oppSkillDamage = fireSkills[oppChooseSkill].damage * 2
+        oppTotalDamage = totalDamageCount(pokemonlist[oppPokemon].attack, pokemonlist[myPokemon].defence, oppSkillDamage)
       } else {
-        oppSkillDamage = grassSkills[oppChooseSkill].damage
+        oppSkillDamage = fireSkills[oppChooseSkill].damage
+        oppTotalDamage = totalDamageCount(pokemonlist[oppPokemon].attack, pokemonlist[myPokemon].defence, oppSkillDamage)
       }
-      myPokemonHp -= oppSkillDamage
-    } else if (pokemonlist[oppChoose].type === 'Water') {
+      myPokemonHp -= oppTotalDamage
+    } else if (pokemonlist[oppPokemon].type === 'Water') {
       let oppChooseSkill = Math.floor(Math.random() * waterSkills.length)
-      console.log(pokemonlist[oppChoose].name + ' attack with ' + waterSkills[oppChooseSkill].skill + '.')
-      if (pokemonlist[oppChoose].type === 'Fire') {
-        oppSkillDamage = grassSkills[oppChooseSkill].damage * 2
+      console.log(pokemonlist[oppPokemon].name + ' attack with ' + waterSkills[oppChooseSkill].skill + '.')
+      if (pokemonlist[myPokemon].type === 'Fire') {
+        oppSkillDamage = waterSkills[oppChooseSkill].damage * 2
+        oppTotalDamage = totalDamageCount(pokemonlist[oppPokemon].attack, pokemonlist[myPokemon].defence, oppSkillDamage)
       } else {
-        oppSkillDamage = grassSkills[oppChooseSkill].damage
+        oppSkillDamage = waterSkills[oppChooseSkill].damage
+        oppTotalDamage = totalDamageCount(pokemonlist[oppPokemon].attack, pokemonlist[myPokemon].defence, oppSkillDamage)
       }
-      myPokemonHp -= oppSkillDamage
+      myPokemonHp -= oppTotalDamage
     }
 
     if (oppSleepSkill != 'Sleep powder') {
@@ -184,18 +203,52 @@ while (myPokemonHp > 0 && oppPokemonHp > 0) {
       console.log(pokemonlist[myPokemon].name + ' has fall asleep and missed a chance!')
       myTurn = false
     }
-    console.log('----------------------------------------------------------------------------')
+    console.log('-----------------------------------------------------')
   }
+}
+
+if (myPokemonHp <= 0) {
+  console.log(pokemonlist[myPokemon].name + ' is defeated by ' + pokemonlist[oppPokemon].name + '.')
+  console.log('You have lose.')
+  console.log('----------------------Game Over----------------------')
+} else {
+  console.log(pokemonlist[oppPokemon].name + ' is defeated by ' + pokemonlist[myPokemon].name + '.')
+  console.log('You have win and gain experience.')
+  console.log('----------------------Game Over----------------------')
 }
 
 function listPokemon(array) {
   for (let i = 0; i < array.length; i++) {
-    console.log(i + '- ' + array[i].name + '\n' + array[i].type)
+    console.log(i + '- ' + array[i].name + '-' + array[i].type)
   }
 }
 
 function listSkills(array) {
   for (let i = 0; i < array.length; i++) {
     console.log(i + '- ' + array[i].skill)
+  }
+}
+
+function listSkillsType(playerType) {
+  switch (playerType) {
+    case 'Grass':
+      listSkills(grassSkills)
+      break;
+    case 'Fire':
+      listSkills(fireSkills)
+      break;
+    case 'Water':
+      listSkills(waterSkills)
+      break;
+  }
+}
+
+function totalDamageCount(Pokemon1Atk, Pokemon2Def, skillDamage) {
+  if (Pokemon1Atk > Pokemon2Def) {
+    return skillDamage + Pokemon1Atk
+  } else if (Pokemon1Atk < Pokemon2Def) {
+    return skillDamage + Pokemon1Atk - Pokemon2Def
+  } else {
+    return skillDamage
   }
 }
